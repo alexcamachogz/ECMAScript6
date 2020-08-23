806b839cb2128878de350d15f459f055d01e56a6
Alejandra Camacho <camacho.ale94@gmail.com>
2020-08-23 11:51:08 -0500

    ES7

---
+++ src/es7/index.js
@@ -0,0 +1,14 @@
+// Includes
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(7)) {
+	console.log('Found Value');
+} else {
+	console.log('Not found');
+}
+
+// Pow
+let base = 4;
+let exponent = 4;
+let result = base ** exponent
+console.log(result);
---
+++ src/es7/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+let base = 4;
+let exponent = 4;
+let result = base ** exponent
+console.log(result);
